c7d706656794c6012e2f8f384e8b83c2387a4b6a
bibugo <bibugo@gmail.com>
2021-04-22 14:28:57 +0800

    Update cnls2adgh.sh

--- cnls2adgh.sh
+++ cnls2adgh.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-SOURCE="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+SOURCE1="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/accelerated-domains.china.conf"
+SOURCE2="https://raw.githubusercontent.com/felixonmars/dnsmasq-china-list/master/apple.china.conf"
 OUTPUT="upstream_dns_file.txt"
 DNS="202.96.128.86"
 
-curl -4sSkL $SOURCE | awk -F "\/" '{print $2}' | {
+curl -4sSkL $SOURCE1 && curl -4sSkL $SOURCE2 | awk -F "\/" '{print $2}' | {
     echo -n "[/" > $OUTPUT
     while read line; do
         echo -n "${line}/" >> $OUTPUT
